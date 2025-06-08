55c5f8673810062ef8cc16d43e52e5f6d24529b8
malik672 <aremumalik05@gmail.com>
2025-06-08 11:50:49 +0100

    block

--- benches/benches/criterion.rs
+++ benches/benches/criterion.rs
@@ -31,9 +31,9 @@ fn parser_benches(c: &mut Criterion) {
                         format!("{sname}/{pname}/{id}")
                     }
                 };
-                if parser.can_lex() {
-                    g.bench_function(mk_id("lex"), |b| b.iter(|| parser.lex(src)));
-                }
+                // if parser.can_lex() {
+                //     g.bench_function(mk_id("lex"), |b| b.iter(|| parser.lex(src)));
+                // }
                 g.bench_function(mk_id("parse"), |b| b.iter(|| parser.parse(src)));
             }
             eprintln!();
